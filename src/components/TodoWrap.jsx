3c360c74c01b9fa93be5661b2b8a9ed276cbaa8f
Michael <51073271+michaeldwihadi@users.noreply.github.com>
2020-09-05 16:06:58 +0800

    completing all the CRUD functionalities

--- src/components/TodoWrap.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-const TodoWrap = ({ children }) => {
-  return <div className="todoList">{children}</div>;
-};
-
-export default TodoWrap;
